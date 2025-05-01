c0a56223aec104596341249498a67c5995257695
Hsu Hsu Zin Naing <NaingH@cardiff.ac.uk>
2025-05-01 01:19:57 +0100

    working relational database with product category

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-from models import db, MenuItem, BasketItem
+from models import db, MenuItem, BasketItem, ProductTypes
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Required for sessions
@@ -13,15 +13,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # db = SQLAlchemy()
 db.init_app(app)
 
-# # MenuItem model
 # class MenuItem(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100))
-#     price = db.Column(db.Float)
-#     description = db.Column(db.String(200))
-#     category = db.Column(db.String(50))
-#     image_url = db.Column(db.String(100))  # e.g. images/1.png
-#     carbon_impact = db.Column(db.Float)
+#     name = db.Column(db.String(100), nullable=False)
+#     description = db.Column(db.Text)
+#     price = db.Column(db.Float, nullable=False)
+#     image_url = db.Column(db.String(255), nullable=False)
+#     category = db.Column(db.String(50)) 
 
 with app.app_context():
     db.create_all()  # Create all tables if they don't exist
@@ -31,7 +29,8 @@ with app.app_context():
 def index():
     menu_items_py = MenuItem.query.all()  # Fetch all menu items
     print(menu_items_py)  # Debugging line to check the fetched items
-    return render_template('index.html', menu_items = menu_items_py)
+    product_types = ProductTypes.query.all()  # Fetch all product types
+    return render_template('index.html', menu_items = menu_items_py, product_types=product_types)
 
 # Add item to the basket
 @app.route('/add_to_basket/<int:item_id>')
--- models.py
+++ models.py
@@ -5,11 +5,18 @@ db = SQLAlchemy()
 
 @dataclass
 class MenuItem(db.Model):
+    __tablename__ = 'menu_item'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text)
     price = db.Column(db.Float, nullable=False)
     image_url = db.Column(db.String(255), nullable=False)
+
+    categoryid = db.Column(db.ForeignKey('product_types.id'), nullable=False)
+    category = db.relationship("ProductTypes", backref=db.backref("menu_items", lazy=True))
+
+    def __str__(self):
+        return f"{self.name} / {self.price} / {self.categoryid} / {self.description} / {self.image_url}"
 
 @dataclass
 class BasketItem(db.Model):
@@ -18,3 +25,13 @@ class BasketItem(db.Model):
     quantity = db.Column(db.Integer, default=1)
 
     menu_item = db.relationship("MenuItem")
+
+@dataclass
+class ProductTypes(db.Model):
+    __tablename__ = 'product_types'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    category = db.Column(db.String(255))
+    description = db.Column(db.Text)
+
+    def __str__(self):
+        return f"{self.category} / {self.description}"
